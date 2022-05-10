c70358c782d0caa5f76932aa7ca5c02e8a6a01fa
Elias <eliasejump@gmail.com>
2022-05-10 18:27:47 +0300

    add: UI

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,51 @@
-require('./bootstrap');
+require('./bootstrap')
+
+let form = document.getElementById('form')
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+    const x = event.target.elements.X
+    const y = event.target.elements.Y
+
+    if (hasEmptyFields([x, y]))
+        return false
+
+    request(x, y)
+})
+
+function hasEmptyFields(array) {
+    let hasErrors = false;
+    for (const element of array) {
+        if (element.value === '') {
+            setError(element, 'Пустое поле')
+            hasErrors = true;
+        }
+    }
+    return hasErrors;
+}
+
+function setError(element, text) {
+    const elementError = element.nextElementSibling.children[0]
+    elementError.innerHTML = text
+    element.classList.add("input-error")
+    element.addEventListener('input', () => clearError(element), {once: true})
+}
+
+function clearError(element) {
+    const elementError = element.nextElementSibling.children[0]
+    elementError.innerHTML = ''
+    element.classList.remove("input-error")
+}
+
+function request(x, y) {
+    const resultElement = document.getElementById('result')
+    resultElement.classList.add('loading')
+
+    const route = `/api/slice?x=${x}&y=${y}`
+
+    let req = new XMLHttpRequest();
+    req.open("GET", "route", true);
+    req.onload = function (){
+        alert( req.status);
+    }
+    req.send(null);
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js('resources/js/app.js', 'public/js').version();
+mix.postCss('resources/css/app.css', 'public/css').version();
+mix.postCss('resources/css/normalize.css', 'public/css').version();
+mix.postCss('resources/css/loader.css', 'public/css').version();
