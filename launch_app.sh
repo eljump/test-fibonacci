98c659bced3b4b20a8934d7eb057d304c518c00a
Elias <eliasejump@gmail.com>
2022-05-14 22:01:16 +0300

    test: hot-fix

--- launch_app.sh
+++ launch_app.sh
@@ -3,8 +3,7 @@
 composer install
 tr -d '\r' <.env.example >env.temp && mv env.temp .env.example
 cp .env.example .env
-alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'
-sail up -d
-sail artisan key:generate
-sail npm install
-sail artisan migrate
+./vendor/bin/sail up -d
+./vendor/bin/sail artisan key:generate
+./vendor/bin/sail npm install
+./vendor/bin/sail artisan migrate
